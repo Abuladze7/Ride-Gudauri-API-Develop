7627399cfc7612c349b40f089d7db99cfa5ac118
Nikoloz Abuladze <146086037+Abuladze7@users.noreply.github.com>
2024-07-31 13:33:50 +0400

    Update ourStoryControllers.js

--- controllers/ourStoryControllers.js
+++ controllers/ourStoryControllers.js
@@ -120,7 +120,7 @@ exports.updateHowStartedSection = async (req, res) => {
 
     if (!section) return res.status(404).json({ message: "Section not found" });
 
-    if (image) {
+    if (image && section.image.public_id) {
       const imgId = section.image.public_id;
       if (imgId) {
         await cloudinary.uploader.destroy(imgId);
