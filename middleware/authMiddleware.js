90ea9a68ad170ffa8ee0e6f440777b19bc609a1f
Nikoloz Abuladze <146086037+Abuladze7@users.noreply.github.com>
2024-08-08 10:31:38 +0400

    updates

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('../config/keys');
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/keys");
 
 const auth = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
+  const token = req.header("Authorization").replace("Bearer ", "");
 
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, jwtSecret);
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
 };
 
 module.exports = auth;
