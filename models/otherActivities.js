e1feed9ee8535c3270f75a5edac4ef23aeefd00e
Nikoloz Abuladze <146086037+Abuladze7@users.noreply.github.com>
2024-08-12 11:21:15 +0400

    added more templates

--- models/otherActivities.js
+++ models/otherActivities.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const otheractivitiesBookingSchema = new mongoose.Schema({
-    type: { type: String, required: true },
-    fullName: { type: String, required: true },
-    number: { type: String, required: true },
-    email: { type: String, required: true },
-    participants: { type: String, required: true },
-    date: { type: Date, required: true },
-    time: { type: String, required: true },
-    selector: { type: String},
-    currency: { type: String },
-    orderTime: { type: Date },
-    additionalDetails: { type: String }
+  type: { type: String, required: true },
+  fullName: { type: String, required: true },
+  number: { type: String, required: true },
+  email: { type: String, required: true },
+  participants: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  selector: { type: String },
+  currency: {
+    usd: Number,
+    gel: Number,
+  },
+  orderTime: { type: Date },
+  additionalDetails: { type: String },
 });
 
-module.exports = mongoose.model('otheractivitiesBooking', otheractivitiesBookingSchema);
+module.exports = mongoose.model(
+  "otheractivitiesBooking",
+  otheractivitiesBookingSchema
+);
