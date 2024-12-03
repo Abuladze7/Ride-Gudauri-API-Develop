b16783809685de73e843f78604849c323b572c5d
Nikoloz Abuladze <146086037+Abuladze7@users.noreply.github.com>
2024-12-03 11:49:25 +0400

    Update skischoolRoutes.js

--- routes/skischoolRoutes.js
+++ routes/skischoolRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createskischoolBooking, getskischoolBooking, updateSkischoolBooking, deleteSkischoolBooking } = require('../controllers/skischoolController');
+const { createskischoolBooking, getskischoolBooking, updateSkischoolBooking, deleteSkischoolBooking, bookingStatus } = require('../controllers/skischoolController');
+
+router.post("/bookingstatus", bookingStatus);
 
 router.post('/', createskischoolBooking);
 router.get('/', getskischoolBooking);
